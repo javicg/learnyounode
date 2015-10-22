af4807f88edafe2b6ccb1029f0834ff100f6cd62
Javi Carretero <fracargar@gmail.com>
2015-10-22 08:07:31 +0100

    Improve solution to exercise #9: use arrays to store the data

--- 9_JugglingAsync/program.js
+++ 9_JugglingAsync/program.js
@@ -1,53 +1,30 @@
 var http = require('http')
 var bl = require('bl')
-
-var url1 = process.argv[2]
-var url2 = process.argv[3]
-var url3 = process.argv[4]
-
+var results = []
 var numHandled = 0
-var dataUrl1 = ""
-var dataUrl2 = ""
-var dataUrl3 = ""
 
-function printResults() {
-	console.log(dataUrl1)
-	console.log(dataUrl2)
-	console.log(dataUrl3)
+// Main program
+for (var i=0; i < 3; i++) {
+	httpGetIndex(i)
 }
 
-http.get(url1, function callback(response) {
+// Aux functions
+function httpGetIndex(index) {
+	http.get(process.argv[2 + index], function callback(response) {
 	response.pipe(bl(function (err, data) {
 		if (err)
 			return console.error(err)
-		dataUrl1 = data.toString()
+		results[index] = data.toString()
 		numHandled++
 		if (numHandled == 3) {
 			printResults()
 		}
 	}))
 })
+}
 
-http.get(url2, function callback(response) {
-	response.pipe(bl(function (err, data) {
-		if (err)
-			return console.error(err)
-		dataUrl2 = data.toString()
-		numHandled++
-		if (numHandled == 3) {
-			printResults()
-		}
-	}))
-})
-
-http.get(url3, function callback(response) {
-	response.pipe(bl(function (err, data) {
-		if (err)
-			return console.error(err)
-		dataUrl3 = data.toString()
-		numHandled++
-		if (numHandled == 3) {
-			printResults()
-		}
-	}))
-})
+function printResults() {
+	for (var i=0; i < 3; i++) {
+		console.log(results[i])
+	}
+}
