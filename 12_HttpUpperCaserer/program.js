6ce3a3be6ccb4101d0eba641e4fff4706eb0dae4
Javi Carretero <fracargar@gmail.com>
2015-10-26 21:45:20 +0000

    Refine solution to exercise #12: return generic message if request is not a POST, and specify correct conte-type on the response

--- 12_HttpUpperCaserer/program.js
+++ 12_HttpUpperCaserer/program.js
@@ -2,11 +2,12 @@ var http = require('http')
 var map = require('through2-map')
 
 var server = http.createServer(function (req, res) {
-	if (req.method == 'POST') {
-		res.writeHead(200, {'content-type': 'text/plain'})
-		req.pipe(map(function (chunk) {
-			return chunk.toString().toUpperCase()
-		})).pipe(res)
-	}
+	if (req.method != 'POST')
+		return res.end('Please, send a POST request!')
+	
+	res.writeHead(200, {'content-type': req.headers['content-type']})
+	req.pipe(map(function (chunk) {
+		return chunk.toString().toUpperCase()
+	})).pipe(res)
 })
 server.listen(Number(process.argv[2]))
